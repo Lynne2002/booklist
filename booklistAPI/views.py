ece19d8b77dcdb6973bdbc88327efe7761d066b0
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-23 11:37:21 +0300

    feat: add code for allowing POST requests

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['POST'])
 def books(request):
     return Response('List of the books',
                     status=status.HTTP_200_OK)

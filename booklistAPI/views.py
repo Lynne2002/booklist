886d056345671923ca2554c9902cf2ee1d8ee8aa
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-23 12:31:58 +0300

    feat: add BookView class that extends viewsets

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, APIView
 from django.http import HttpResponse
+from rest_framework import viewsets
 
 @api_view(['GET','POST'])
 def books(request):
@@ -20,3 +21,22 @@ class BookView(APIView):
         return Response({"message": "single book with ID "+str(pk)}, status.HTTP_200_OK)
     def put(self, request,pk):
         return Response({"title:", request.data.get('title')}, status.HTTP_200_OK)
+    
+class BookView_viewsets(viewsets.ViewSet):
+    def list(self, request):
+        return Response({"message": "All books"}, status.HTTP_200_OK)
+    
+    def create(self, request):
+        return Response({"message": "Creating a book"}, status.HTTP_201_CREATED)
+    
+    def update(self, request, pk=None):
+        return Response({"message": "Updating a book"}, status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        return Response({"message": "Displaying a book"}, status.HTTP_200_OK)
+    
+    def partial_update(self, request, pk=None):
+        return Response({"message": "Partially updating a book"}, status.HTTP_200_OK)
+    
+    def destroy(self, request, pk=None):
+        return Response({"message": "Deleting a book"}, status.HTTP_200_OK)

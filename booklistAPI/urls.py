d2dbf9a2d8157f55e9ac38158d95be6183c4515c
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-23 12:32:28 +0300

    feat: add routing for BookView_viewsets class

--- booklistAPI/urls.py
+++ booklistAPI/urls.py
@@ -4,4 +4,18 @@ urlpatterns =[
     path('books/', views.books),
     path('orders', views.Orders.listOrders),
     path('books/<int:pk>', views.BookView.as_view()),
+    path('books_viewsets', views.BookView_viewsets.as_view(
+        {
+            'get': 'list',
+            'post': 'create',
+        }
+    )),
+    path("books_viewsets/<int:pk>", views.BookView_viewsets.as_view(
+        {
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+        }
+    ))
 ]

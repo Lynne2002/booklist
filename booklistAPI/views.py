65fed68019f9d828955202e3a3eff5d49b5ddf3a
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-23 12:08:37 +0300

    feat: route class based BookView

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 from django.http import HttpResponse
 
 @api_view(['GET','POST'])
@@ -15,3 +15,8 @@ class Orders():
     def listOrders(request):
         return Response({'message':'list of orders'},200)
 
+class BookView(APIView):
+    def get(self, request, pk):
+        return Response({"message": "single book with ID "+str(pk)}, status.HTTP_200_OK)
+    def put(self, request,pk):
+        return Response({"title:", request.data.get('title')}, status.HTTP_200_OK)

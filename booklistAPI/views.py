7f7371460c30200ddae1e857d24848893bdc29f9
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-23 11:42:12 +0300

    feat: use HttpResponse instead of rest_framework
    to compare how the two responses differ  - how rest_framework response is better

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
-@api_view(['POST'])
+#@api_view(['POST'])
 def books(request):
-    return Response('List of the books',
+    return HttpResponse('List of the books',
                     status=status.HTTP_200_OK)
 

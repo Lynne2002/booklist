4456f630934c6f50580dc53be0fdb71a7f083ff6
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-24 10:01:47 +0300

    fix: import APIView from rest_framework.views
    Not from rest_framework.decorators

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, APIView
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from django.http import HttpResponse
 from rest_framework import viewsets
 
